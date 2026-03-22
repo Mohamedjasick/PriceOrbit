92224f24c45cc9ba1506608b339a0ec69d8c58c0
Mohamedjasick <mohamedjasick123@gmail.com>
2026-03-22 11:47:29 +0530

    Ready for Vercel deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,3 @@
-const config = {
-  API_BASE_URL: process.env.REACT_APP_API_URL || "http://localhost:8080"
-};
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
-export default config;
+export default API_BASE_URL;
